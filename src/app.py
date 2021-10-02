9238770129bc4b77515e85b305a4f261646acda8
korulis <broliukas@gmail.com>
2021-10-02 12:55:20 +0300

    Add env vars support.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,21 @@
+import os
 from flask import Flask
 from flask.json import jsonify
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+class conf:
+    __env_path = os.getcwd() + "/.env"
+    __env_path_local = os.getcwd() + "/.env.local"
+    load_dotenv(dotenv_path=__env_path, override=True)
+    load_dotenv(dotenv_path=__env_path_local, override=True)
+    some_config = os.environ["POSTGRES_PASSWORD"]
 
 
 @app.route("/status")
 def status():
+    print(conf.some_config)
     return jsonify({"status": "Ok!"})
 
 
