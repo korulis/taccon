86906ef83701fdbee63e2d16756eb71e3336ee42
korulis <broliukas@gmail.com>
2021-10-02 12:20:20 +0300

    Add alembic migration.

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Backend for tic-tac-toe
 - `python -m venv env`
 - `source env/Scripts/activate`
 - `pip install -r requirements.txt`
+- `docker run -d -p 5432:5432 --name tac-db -e POSTGRES_PASSWORD=pass1234 postgres:10.13-alpine`
 - `python src/app.py`
---
+++ alembic/versions/5af82f2dbe81_moves_table.py
@@ -0,0 +1,24 @@
+"""moves table
+
+Revision ID: 5af82f2dbe81
+Revises: 
+Create Date: 2021-10-02 11:40:27.139442
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5af82f2dbe81'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
